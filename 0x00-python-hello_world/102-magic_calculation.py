0bf633fdb7ba5ccb5ff58696f56a2700c0e400e5
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-09-06 23:57:47 -0500

    Up

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# function that does exactly the same as the following Python bytecode
+# function that does exactly
+#the same as the following Python bytecode
 
 def magic_calculation(a, b):
 	return (98 + a**b)
