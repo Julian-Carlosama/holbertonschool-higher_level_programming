1309f77842cd05547b13c9be346216377d20623c
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-09-12 22:38:36 -0500

    V3

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,30 +4,24 @@ if __name__ == "__main__":
     import sys
 
     dat = len(sys.argv) - 1
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[-1])
-    suma = calculator_1.add(num1, num2)
-    resta = calculator_1.sub(num1, num2)
-    multi = calculator_1.mul(num1, num2)
-    div = calculator_1.div(num1, num2)
 
-    if dat == 3:
-        op = sys.argv[2]
-        if op == "+":
-            print("{} + {} = {}".format(num1, num2, suma))
-            exit(0)
-        elif op == "-":
-            print("{} - {} = {}".format(num1, num2, resta))
-            exit(0)
-        elif op == "*":
-            print("{} * {} = {}".format(num1, num2, multi))
-            exit(0)
-        elif op == "/":
-            print("{} / {} = {}".format(num1, num2, div))
-            exit(0)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-    else:
+    if dat != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    elif dat == 3:
+        num1 = int(sys.argv[1])
+        num2 = int(sys.argv[3])
+        from calculator_1 import add, sub, mul, div
+        op = sys.argv[2]
+        if op == "+":
+            print("{} + {} = {}".format(num1, num2, add(num1, num2)))
+            exit(0)
+        elif op == "-":
+            print("{} - {} = {}".format(num1, num2, sub(num1, num2)))
+            exit(0)
+        elif op == "*":
+            print("{} * {} = {}".format(num1, num2, mul(num1, num2)))
+            exit(0)
+        elif sys.argv[2] == "/":
+            print("{} / {} = {}".format(num1, num2, div(num1, num2)))
+            exit(0)
