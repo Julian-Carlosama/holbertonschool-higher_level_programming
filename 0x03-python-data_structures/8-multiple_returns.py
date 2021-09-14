3a10ca68e18d36a60f97024bf382d1dfe3a6c797
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-09-13 23:15:56 -0500

    8. More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    len_str = len(sentence)
+
+    if len_str == 0:
+        new_t = (len_str, none)
+    else:
+        new_t = (len_str, sentence[0])
+    return(new_t)
