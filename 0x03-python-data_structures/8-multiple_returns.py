07beef3be33045236799fedc0ab8aacde7efe180
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-09-13 23:24:50 -0500

    V2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     len_str = len(sentence)
 
     if len_str == 0:
-        new_t = (len_str, none)
+        new_t = (len_str, None)
     else:
         new_t = (len_str, sentence[0])
     return(new_t)
