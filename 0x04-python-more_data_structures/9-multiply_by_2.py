4e8d88062dd96e5f99c05b9f37d922d52498b054
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-09-16 23:40:10 -0500

    V2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dic = a_dictionary.copy()
-    for i in a_dictionary:
-        new_dic[i] = a_dictionary[i]
+    for i in new_dic:
+        new_dic[i] *= 2 #a_dictionary[i]
     return (new_dic)
