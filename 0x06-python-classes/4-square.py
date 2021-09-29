418529ecb090c8c14c3aacc86941788ee2f543f4
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-09-28 23:55:08 -0500

    ALL Classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Class that defines a square """
+
+
+class Square:
+    """ Class Square """
+    def __init__(self, size=0):
+        """ Method that inicialized an object """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+    def area(self):
+        """ returns the current square area """
+        return(self.__size ** 2)
+
+    @property
+    def size(self):
+        """ Method that set of the object """
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ Method """
+        self.__size = int(size)
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
