e93f2d9c6abf939b3293655da2efa83e7df5041b
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-10-07 02:24:34 -0500

    V3.2

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,28 +6,35 @@ class Square:
     """ Class Square """
     def __init__(self, size=0):
         """ Method that inicialized an object """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
-
-    def area(self):
-        """ returns the current square area """
-        return(self.__size ** 2)
+        width = 0
+        height = 0
 
     @property
-    def size(self):
-        """ Method that set of the object """
-        return(self.__size)
+    def width(self):
+        """ Method that return the width  """
+        return(self.__width)
 
-    @size.setter
-    def size(self, value):
-        """ Method """
+    @width.setter
+    def width(self, value):
+        """ Method that allow create a width """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value
+            self.__width = value
+
+    @property
+    def height(self):
+        """ Method that return the height """
+        return(self.__height)
+
+    @width.setter
+    def height(self, value):
+        """ Method that allow create a height """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__height = value
