8e90b7fefd8bc88e2732b65ee1506408f090d6cf
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-10-07 02:22:25 -0500

    V2 Conditionals

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,8 +9,12 @@ def add_integer(a, b=98):
     Return:
          Addition of two numbers
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return(int(a + b))
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return(a + b)
