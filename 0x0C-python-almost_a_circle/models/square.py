a3a48f2ab4a3fd652850cafcac237925f7273c3c
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-10-20 16:58:32 -0500

    V1

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" """
+
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """ Init constructor or magic method """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Call attributes  """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Method should return [Square] (<id>) <x>/<y> - <size>"""
+        id_r = f"({self.id}) "
+        x_y_r = f"{self.x}/{self.y} - "
+        wid_hei = f"{self.width}"
+
+        return ("[Square] " + id_r + x_y_r + wid_hei)
