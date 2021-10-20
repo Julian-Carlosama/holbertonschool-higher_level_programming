08d9a2f66fafb9f1717a6411fd519274fd4c85a2
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-10-20 17:01:02 -0500

    V2 documentantion

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-""" """
+"""
+Class square
+ """
 
 
 from models.rectangle import Rectangle
+"""
+Import class rectangle 
+"""
 
 class Square(Rectangle):
     """ Init constructor or magic method """
