b7fde1a3fdcfcdb8fb4d66b9419123dd4dcfc8a4
Julian-Carlosama <67349003+Julian-Carlosama@users.noreply.github.com>
2021-11-18 23:03:10 -0500

    V2 updated uppercase words

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
--- SHow all the cities of California that
+-- Show all the cities of California that
 -- can be found in the database hbtn_0d_usa.
-SELECT id, name From cities Where state_id = (
+SELECT id, name FROM cities WHERE state_id = (
 SELECT id FROM states WHERE = name = "California");
