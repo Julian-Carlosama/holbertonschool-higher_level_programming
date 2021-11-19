9c8cfee7197fc7d75e1cc466d89835ce79aa6ccf
Julian-Carlosama <67349003+Julian-Carlosama@users.noreply.github.com>
2021-11-18 19:35:34 -0500

    V2 updated syntax

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Show all records of the table second_table of the database hbtn_0c_0
-SELECT score, name FROM second-table WHERE name IS NOT NULL BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL BY score DESC;
