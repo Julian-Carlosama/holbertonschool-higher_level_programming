d648be8d5f749f826ec3095a499179a29fa5d166
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-14 00:20:40 -0500

    V4 Update semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-  let result = parseInt(a) + parseInt(b);
+function add (a, b) {
+  const result = parseInt(a) + parseInt(b);
   console.log(result);
 }
 add(process.argv[2], process.argv[3]);
