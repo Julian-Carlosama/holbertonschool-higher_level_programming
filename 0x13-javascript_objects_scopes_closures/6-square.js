a8f36162a5d4ac2cdd43d893603ba2dce8ad7764
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-15 01:41:31 -0500

    V2 Update script

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,18 @@
 #!/usr/bin/node
-let Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super (size, size);
+const square5 = require('./5-square');
+class Square extends square5 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let squ = '';
+        for (let j = 0; j < this.width; j++) {
+          squ += 'c';
+        }
+        console.log(squ);
+      }
+    }
   }
 }
 module.exports = Square;
