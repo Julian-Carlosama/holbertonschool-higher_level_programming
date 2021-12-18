06c4cf62d322328b240699a6bb81fdaf0d8eaa3d
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-18 18:59:27 -0500

    V1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa:
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "main":
+    """ Values for the connect to the database """
+    users = sys.argv[1]
+    passw = sys.argv[2]
+    datB = sys.argv[3]
+
+    """ Configuration to the database """
+    db = MySQLdb.connect(host="localhost", user=users, passwd=passw, db=datB,
+                         port=3306, charset="utf-8")
+
+    """
+    Class used to execute the database whit python,
+    It gives us the ability to have multiple seperate working environments
+    through the same connection to the database.
+    """
+    cur = db.cursor()
+
+    """ Query used for execute records of the table """
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+
+    """ Get all records from the cursor object"""
+    states = cur.fetchall()
+
+    """ Show the results find """
+    for state in states:
+        print(state)
