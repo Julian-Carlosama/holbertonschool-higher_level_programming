957dc44e42effe226a73bfe8568931b6b0c10a89
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-19 17:50:18 -0500

    V1

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """ Values for the connect to the database """
+    users = sys.argv[1]
+    passw = sys.argv[2]
+    datB = sys.argv[3]
+
+    """Connecting to the database"""
+    db = MySQLdb.connect(host="localhost", user=users, password=passw, db=datB,
+                         port=3306, charset="utf8")
+
+    """The cursor gives us the ability to have multiple seperate
+    working environments through the same connection to the database"""
+    cur = db.cursor()
+
+    """Executing the query"""
+    cur.execute("SELECT cities.id, cities.name, states.name \
+FROM cities JOIN states ON states.id = cities.state_id \
+ORDER BY cities.id ASC")
+
+    stat = cur.fetchall()
+
+    """ Show the results find """
+    for state in stat:
+        print(state)
+    cur.close()
+    """Closing the connection to the database"""
+    db.close()
