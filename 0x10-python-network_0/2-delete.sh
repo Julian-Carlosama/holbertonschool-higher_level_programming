5c43123226a8553a26c1e180a0731c6196ba9442
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-01-09 18:58:12 -0500

    V2 all scripts update

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#
+#Script that sends a DELETE request to the URL passed as the first argument and
+curl -s "$1" -X DELETE
+
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#
+#Script that takes in a URL as an argument, sends a GET request to the URL
+
+curl -sH "X-School-User-Id 98" "$1"
