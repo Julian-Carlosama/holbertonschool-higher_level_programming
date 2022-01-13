f640d5d1d58d85b2ef9a977a0da0a443342a8f74
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-01-13 02:11:52 -0500

    V1 More scripts

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Script that fetches a website """
+
+
+import requests
+
+
+if __name__ == "__main__":
+    requ = requests.get('https://intranet.hbtn.io/status')
+    print('Body Response:')
+    print('\t- type:', type(requ.text))
+    print('\t- content:', requ.text)
