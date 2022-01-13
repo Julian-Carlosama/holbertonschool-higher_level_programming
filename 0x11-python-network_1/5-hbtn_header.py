4063db160ff1c85f8289cb01f51dd6bd37802d83
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-01-13 02:23:51 -0500

    V2 fix the variable X

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,4 +12,4 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-    print(req.headers.get('X-Request_Id'))
+    print(req.headers.get('X-Request-Id'))
