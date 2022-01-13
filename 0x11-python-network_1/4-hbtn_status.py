e4b22fe461541fefeff754a48e5b3ec4f89d5921
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-01-13 02:18:56 -0500

    V2 fix print

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,6 @@ import requests
 
 if __name__ == "__main__":
     requ = requests.get('https://intranet.hbtn.io/status')
-    print('Body Response:')
-    print('\t- type:', type(requ.text))
-    print('\t- content:', requ.text)
+    print('Body response:')
+    print('\t- type: {}'.format(type(requ.text)))
+    print('\t- content: {}'.format(requ.text))
